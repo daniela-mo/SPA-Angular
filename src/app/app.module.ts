75dae71e0db0681c347f881acae040ce07552001
Daniela Oliveira <danny.oliver94@gmail.com>
2021-08-31 01:49:58 -0300

    Teste Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { WrapperComponent } from "./wrapper/wrapper.component";
+import { RecursosComponent } from "./recursos/recursos.component";
+import { ServicosComponent } from "./servicos/servicos.component";
+import { NegocioComponent } from "./negocio/negocio.component";
+import { ComentsComponent } from "./coments/coments.component";
+import { ContatoComponent } from "./contato/contato.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WrapperComponent,
+    RecursosComponent,
+    ServicosComponent,
+    NegocioComponent,
+    ComentsComponent,
+    ContatoComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
